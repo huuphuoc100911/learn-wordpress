72037c1b9add7a0a7ade7a36143e5c065335698c
huuphuoc100911 <nguyenvanhuuphuoc17t1@gmail.com>
2024-03-14 11:31:09 +0700

    Show columns and value of post type and taxonomy

--- wp-content/plugins/wp-me-ecommerce/includes/includes.php
+++ wp-content/plugins/wp-me-ecommerce/includes/includes.php
@@ -5,3 +5,6 @@ include_once WP_ME_PATH . 'includes/post_types.php';
 
 // Đăng ký metaboxs
 include_once WP_ME_PATH . 'includes/metaboxes.php';
+
+// Thêm các cột vào custom post_type và custom taxonomy
+include_once WP_ME_PATH . 'includes/admin_columns.php';
